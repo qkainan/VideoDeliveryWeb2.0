d50fece7a0357bbaa3a7a2aacd8b94d4a6457d86
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-06 17:32:28 +0800

    refactor(security):1.重构了本模块中的JwtUtil

--- springsecurity/src/main/java/com/feidian/utils/JwtUtil.java
+++ springsecurity/src/main/java/com/feidian/utils/JwtUtil.java
@@ -21,13 +21,13 @@ public class JwtUtil {
     //有效期为
     public static final Long JWT_TTL = 60 * 60 *1000L;// 60 * 60 *1000  一个小时
     //设置秘钥明文
-    public static final String JWT_KEY = "sangeng";
+    public static final String JWT_KEY = "maibao";
 
     public static String getUUID(){
         String token = UUID.randomUUID().toString().replaceAll("-", "");
         return token;
     }
-    
+
     /**
      * 生成jtw
      * @param subject token中要存放的数据（json格式）
@@ -49,25 +49,6 @@ public class JwtUtil {
         return builder.compact();
     }
 
-    private static JwtBuilder getJwtBuilder(String subject, Long ttlMillis, String uuid) {
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-        SecretKey secretKey = generalKey();
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-        if(ttlMillis==null){
-            ttlMillis=JwtUtil.JWT_TTL;
-        }
-        long expMillis = nowMillis + ttlMillis;
-        Date expDate = new Date(expMillis);
-        return Jwts.builder()
-                .setId(uuid)              //唯一的ID
-                .setSubject(subject)   // 主题  可以是JSON数据
-                .setIssuer("sg")     // 签发者
-                .setIssuedAt(now)      // 签发时间
-                .signWith(signatureAlgorithm, secretKey) //使用HS256对称加密算法签名, 第二个参数为秘钥
-                .setExpiration(expDate);
-    }
-
     /**
      * 创建token
      * @param id
@@ -80,22 +61,49 @@ public class JwtUtil {
         return builder.compact();
     }
 
-    public static void main(String[] args) throws Exception {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYWM2ZDVhZi1mNjVlLTQ0MDAtYjcxMi0zYWEwOGIyOTIwYjQiLCJzdWIiOiJzZyIsImlzcyI6InNnIiwiaWF0IjoxNjM4MTA2NzEyLCJleHAiOjE2MzgxMTAzMTJ9.JVsSbkP94wuczb4QryQbAke3ysBDIL5ou8fWsbt_ebg";
-        Claims claims = parseJWT(token);
-        System.out.println(claims);
+    private static JwtBuilder getJwtBuilder(String subject, Long ttlMillis, String uuid) {
+        // 指定签名的时候使用的签名算法，也就是header那部分
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        //创建秘钥
+        SecretKey secretKey = generalKey();
+        // 生成JWT的时间
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        //判断是否使用默认超时时间
+        if(ttlMillis==null){
+            ttlMillis=JwtUtil.JWT_TTL;
+        }
+        // 设置过期时间
+        long expMillis = nowMillis + ttlMillis;
+        Date expDate = new Date(expMillis);
+        return Jwts.builder()
+                .setId(uuid)              //唯一的ID
+                .setSubject(subject)   // 主题  可以是JSON数据
+                .setIssuer("sg")     // 签发者
+                .setIssuedAt(now)      // 签发时间
+                .signWith(signatureAlgorithm, secretKey) //使用HS256对称加密算法签名, 第二个参数为秘钥
+                .setExpiration(expDate); // 设置过期时间
     }
+
+
+//    public static void main(String[] args) throws Exception {
+//        String token = createJWT("666");
+//        Claims claims = parseJWT(token);
+//        System.out.println(claims);
+//    }
 
     /**
      * 生成加密后的秘钥 secretKey
      * @return
      */
     public static SecretKey generalKey() {
+        // 本地的密码解码，这里自定义
         byte[] encodedKey = Base64.getDecoder().decode(JwtUtil.JWT_KEY);
+        // 根据给定的字节数组使用AES加密算法构造一个密钥
         SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
         return key;
     }
-    
+
     /**
      * 解析
      *
@@ -109,6 +117,38 @@ public class JwtUtil {
                 .setSigningKey(secretKey)
                 .parseClaimsJws(jwt)
                 .getBody();
+    }
+
+    /**
+     * 从token中获取subject
+     *
+     * @param token
+     * @return
+     * @throws Exception
+     */
+    public static Long getSubject(String token) {
+        try {
+            if (!StringUtils.hasText(token)) return 0L;
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(generalKey()).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            String userId = (String) claims.get("sub");
+            return Long.parseLong(userId);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0L;
+        }
+    }
+
+    public static Long getUserId(){
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest req = sra.getRequest();
+
+        String token = req.getHeader("Authorization");
+        Long userId = com.feidian.util.JwtUtil.getSubject(token);
+        return userId;
     }
 }
 
